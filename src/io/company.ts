175ab4b63a4b6a64da1dc667bf20b198e32f3efc
nandobfer <nandobfer@yahoo.com.br>
2023-10-26 14:46:19 -0300

    update: emition name

--- src/io/company.ts
+++ src/io/company.ts
@@ -5,31 +5,33 @@ const prisma = new PrismaClient();
 
 // FUNÇÃO PARA LISTAR COMPANIAS EXISTENTES
 const list = async (socket: Socket) => {
-  const company = await prisma.company.findMany();
+  const company = await prisma.company.findMany({ include: { customer: true } })
   socket.emit("company:list", company);
 };
 
 // FUNÇÃO PARA CADASTRAR COMPANIAS
 export const register = async (socket: Socket, data: Company) => {
-  try {
-    const user = await prisma.company.create({
-      data: {
-        name: data.name,
-        cnpj: data.cnpj,
-        city: data.city,
-        state: data.state,
-        customerId: data.customerId,
-      },
-    });
+    console.log(data)
+    try {
+        const company = await prisma.company.create({
+            data: {
+                name: data.name,
+                cnpj: data.cnpj,
+                city: data.city,
+                state: data.state,
+                customerId: data.customerId,
+            },
+        })
 
-    if (user) {
-      socket.emit("signup:success", user);
-    } else {
-      socket.emit("signup:error", "Input valid user data");
+        if (company) {
+            socket.emit("company:signup:success", company)
+        } else {
+            socket.emit("company:signup:error", "Input valid company data")
+        }
+    } catch (error) {
+        console.log(error)
+        socket.emit("company:signup:error", error)
     }
-  } catch (error) {
-    socket.emit("signup:error", error);
-  }
 };
 
 export default { list, register };
