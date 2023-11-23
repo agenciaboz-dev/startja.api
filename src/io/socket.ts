a00fd8c5107dfb9ffda7112ed987a5110bb20e41
nandobfer <nandobfer@yahoo.com.br>
2023-11-23 18:21:56 -0300

    clean

--- src/io/socket.ts
+++ src/io/socket.ts
@@ -1,90 +1,51 @@
 import { Socket } from "socket.io";
 import { Server as SocketIoServer } from "socket.io";
 import { Server as HttpServer } from "http";
-import { Server as HttpsServer } from "https";
-import { Client, ClientBag } from "../definitions/client";
+import { Server as HttpsServer } from "https"
+import main from "./main"
 
-import main from "./main";
-
-let io: SocketIoServer | null = null;
-
-export let clientList: Client[] = [];
-
-const get = (socket: Socket) =>
-  clientList.find((client) => client.socket == socket);
-const find = (id: number) => clientList.find((client) => client.user?.id == id);
-const getUser = (client: Client) => client.user;
-const list = () => clientList.map((client) => client.user);
-
-const remove = (client: Client | undefined) => {
-  if (!client) return;
-  clientList = clientList.filter((item) => item.socket != client.socket);
-};
-
-const add = (client: Client) => {
-  const exists = find(client.user?.id);
-  if (exists) remove(client);
-
-  clientList.push(client);
-};
-
-const clients: ClientBag = {
-  get,
-  find,
-  getUser,
-  list,
-  add,
-  remove,
-};
+let io: SocketIoServer | null = null
 
 export const initializeIoServer = (server: HttpServer | HttpsServer) => {
-  io = new SocketIoServer(server, {
-    cors: { origin: "*" },
-    maxHttpBufferSize: 1e8,
-  });
-};
+    io = new SocketIoServer(server, {
+        cors: { origin: "*" },
+        maxHttpBufferSize: 1e8
+    })
+}
 
 export const getIoInstance = () => {
-  if (!io) {
-    throw new Error(
-      "Socket.IO has not been initialized. Please call initializeIoServer first."
-    );
-  }
-  return io;
-};
+    if (!io) {
+        throw new Error("Socket.IO has not been initialized. Please call initializeIoServer first.")
+    }
+    return io
+}
 
 export const handleSocket = (socket: Socket) => {
-  console.log(`new connection: ${socket.id}`);
+    console.log(`new connection: ${socket.id}`)
 
-  socket.on("disconnect", () => {
-    console.log(`disconnected: ${socket.id}`);
-  });
+    socket.on("disconnect", () => {
+        console.log(`disconnected: ${socket.id}`)
+    })
 
-  socket.on("user:login", (data) => main.handleLogin(socket, data));
+    socket.on("user:login", (data) => main.handleLogin(socket, data))
+    socket.on("user:list", (data) => main.userList(socket))
 
-  socket.on("user:list", (data) => main.userList(socket));
+    socket.on("product:list", (data) => main.productList(socket))
+    socket.on("product:create", (data) => main.productCreate(socket, data))
 
-  socket.on("product:list", (data) => main.productList(socket));
+    socket.on("customer:list", (data) => main.customerList(socket))
+    socket.on("customer:signup", (data) => main.handleSignup(socket, data))
 
-  socket.on("product:create", (data) => main.productCreate(socket, data));
+    socket.on("company:list", (data) => main.companyList(socket))
+    socket.on("company:create", (data) => main.companyCreate(socket, data))
 
-  socket.on("customer:list", (data) => main.customerList(socket));
+    socket.on("nature:list", () => main.natureList(socket))
+    socket.on("nature:create", (data) => main.natureCreate(socket, data))
 
-  socket.on("customer:signup", (data) => main.handleSignup(socket, data));
+    socket.on("rule:create", (data) => main.ruleCreate(socket, data))
 
-  socket.on("company:list", (data) => main.companyList(socket));
+    socket.on("property:list", () => main.propertyList(socket))
+    socket.on("property:create", (data) => main.propertyCreate(socket, data))
 
-  socket.on("company:create", (data) => main.companyCreate(socket, data));
-
-  socket.on("nature:list", () => main.natureList(socket));
-
-  socket.on("nature:create", (data) => main.natureCreate(socket, data));
-
-  socket.on("rule:create", (data) => main.ruleCreate(socket, data));
-
-  socket.on("property:list", () => main.propertyList(socket));
-
-  socket.on("property:create", (data) => main.propertyCreate(socket, data));
-
-  socket.on("nota:create", (data) => main.notaCreate(socket, data));
-};
+    socket.on("nota:create", (data) => main.notaCreate(socket, data))
+}
