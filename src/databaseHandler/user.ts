bb080d30097d058b382acf99c1583aa05a0433e3
Lucas Patricio <lpatriciopro@outlook.com>
2023-11-29 11:18:32 -0300

    get user

--- src/databaseHandler/user.ts
+++ src/databaseHandler/user.ts
@@ -8,28 +8,36 @@ const inclusions = {
 };
 
 // Funções relacionadas aos usuários / clientes e admins ⬇️
-  // Funções de login para Admins
-  const loginAdmin = async (data: LoginForm) =>
-      await prisma.admin.findFirst({
-          where: { OR: [{ email: data.login }], AND: { password: data.password } }
-      })
+// Funções de login para Admins
+const loginAdmin = async (data: LoginForm) =>
+  await prisma.admin.findFirst({
+    where: { OR: [{ email: data.login }], AND: { password: data.password } },
+  });
 
-  // Função de login para clientes
-  const loginCustomer = async (data: LoginForm) =>
-      await prisma.customer.findFirst({
-          where: {
-              OR: [{ email: data.login }, { document: data.login }],
-              AND: { password: data.password }
-          },
-          include: inclusions.customer
-      })
-  // Função para listar todos os dados de todos os usuários. ADMINS e CLIENTES
-  const list = async () => {
-      const admin = await prisma.admin.findMany({})
-      const customer = await prisma.customer.findMany({
-          include: inclusions.customer
-      })
-      return { admin, customer }
-  }
+// Função de login para clientes
+const loginCustomer = async (data: LoginForm) =>
+  await prisma.customer.findFirst({
+    where: {
+      OR: [{ email: data.login }, { document: data.login }],
+      AND: { password: data.password },
+    },
+    include: inclusions.customer,
+  });
+// Função para listar todos os dados de todos os usuários. ADMINS e CLIENTES
+const list = async () => {
+  const admin = await prisma.admin.findMany({});
+  const customer = await prisma.customer.findMany({
+    include: inclusions.customer,
+  });
+  return { admin, customer };
+};
 
-export default { loginAdmin, loginCustomer, list }
+const get = async (id: number) =>
+  await prisma.customer.findUnique({
+    where: {
+      id,
+    },
+    include: inclusions.customer,
+  });
+
+export default { loginAdmin, loginCustomer, list, get };
