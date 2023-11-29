9b8c2075c9eb8472b53be4a807acd7ce8765779c
nandobfer <nandobfer@yahoo.com.br>
2023-11-29 17:41:28 -0300

    feat: emitting user update on new notaFiscal

--- src/io/nota.ts
+++ src/io/nota.ts
@@ -1,6 +1,7 @@
 import { Socket } from "socket.io";
 import { NewNota } from "../definitions/userOperations";
 import databaseHandler from "../databaseHandler";
+import { getIoInstance } from "./socket"
 
 const notaList = async (socket: Socket) => {
   try {
@@ -16,6 +17,14 @@ const notaCreate = async (socket: Socket, data: NewNota) => {
   try {
     const nota = await databaseHandler.nota.create(data);
     socket.emit("nota:creation:success", nota);
+
+    const company = await databaseHandler.company.get(nota.companyId)
+    if (company) {
+        const user = await databaseHandler.user.get(company?.customerId)
+        const io = getIoInstance()
+        io.emit("user:update", user)
+    }
+
   } catch (error) {
     console.error(`Error creating nota`, error);
     socket.emit("nota:creation:error", { error });
