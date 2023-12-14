1deeacb196ee46119ea70a76b07f680dbf47f2ec
astriel-git <lpatriciopro@outlook.com>
2023-12-14 16:20:38 -0300

    Pushing everything back

--- src/io/user.ts
+++ src/io/user.ts
@@ -3,18 +3,18 @@ import { LoginForm } from "../definitions/userOperations";
 import databaseHandler from "../databaseHandler";
 
 const handleLogin = async (socket: Socket, data: LoginForm) => {
-  const admin = await databaseHandler.user.loginAdmin(data);
+  const customer = await databaseHandler.user.loginCustomer(data);
 
-  if (admin && admin.password === data.password) {
+  if (customer && customer.password === data.password) {
     // Successfully logged in
-    socket.emit("admin:login:success", admin);
+    socket.emit("customer:login:success", customer);
   } else {
     // Admin login failed, try customer login
-    const customer = await databaseHandler.user.loginCustomer(data);
+    const admin = await databaseHandler.user.loginAdmin(data);
 
-    if (customer && customer.password === data.password) {
+    if (admin && admin.password === data.password) {
       // Successfully logged in as a customer
-      socket.emit("customer:login:success", customer);
+      socket.emit("admin:login:success", admin);
     } else {
       // Both login attempts failed
       socket.emit("user:login:failed", { error: "Credenciais inválidas." });
