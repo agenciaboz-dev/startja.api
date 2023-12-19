17e380da2accff24fe81c6fc4abcc8dc01e24d85
astriel-git <lpatriciopro@outlook.com>
2023-12-19 08:42:29 -0300

    Feat: Function for finding if nfe is "entrada" or "saida" based on CFOP

--- src/io/formatting.ts
+++ src/io/formatting.ts
@@ -6,3 +6,12 @@ const normalize = (string: string) =>
     .replace(/\./g, "");
 
 export default normalize;
+
+const cfopDocType = (data: string) => {
+  const cfopNumber = Number(data);
+  if (cfopNumber >= 1000 && cfopNumber <= 3999) return 0;
+  if (cfopNumber >= 5000 && cfopNumber <= 7999) return 1;
+  return "invalido";
+};
+
+export { cfopDocType };
