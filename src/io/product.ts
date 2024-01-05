70d6a66193a1a9af25bcf2e70ce4c47295616b46
nandobfer <nandobfer@yahoo.com.br>
2024-01-05 15:21:15 -0300

    update: broadcasting new product

--- src/io/product.ts
+++ src/io/product.ts
@@ -1,25 +1,29 @@
-import { Socket } from "socket.io";
-import { NewProduct } from "../definitions/userOperations";
-import databaseHandler from "../databaseHandler";
+import { Socket } from "socket.io"
+import { NewProduct } from "../definitions/userOperations"
+import databaseHandler from "../databaseHandler"
+import { getIoInstance } from "./socket"
 
 const productList = async (socket: Socket) => {
-  try {
-    const product = await databaseHandler.product.list();
-    socket.emit("product:list", product);
-  } catch (error) {
-    console.error(`Error fetching product list`);
-    socket.emit("product:list:error", { error });
-  }
-};
+    try {
+        const product = await databaseHandler.product.list()
+        socket.emit("product:list", product)
+    } catch (error) {
+        console.error(`Error fetching product list`)
+        socket.emit("product:list:error", { error })
+    }
+}
 
 const productCreate = async (socket: Socket, data: NewProduct) => {
-  try {
-    const product = await databaseHandler.product.create(data);
-    socket.emit("product:creation:successful", product);
-  } catch (error) {
-    console.error(`Error creating product`, error);
-    socket.emit("product:creation:error", { error });
-  }
-};
+    try {
+        const product = await databaseHandler.product.create(data)
+        socket.emit("product:creation:successful", product)
 
-export default { productList, productCreate };
+        const io = getIoInstance()
+        io.emit("product:new", product)
+    } catch (error) {
+        console.error(`Error creating product`, error)
+        socket.emit("product:creation:error", { error })
+    }
+}
+
+export default { productList, productCreate }
